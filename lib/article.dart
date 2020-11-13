fc4c6483b62e6cfbf5b4d971b796d35016bf15c0
meirusfandi <me@meirusfandi.com>
2020-11-13 17:50:17 +0700

    initial commit

---
+++ lib/article.dart
@@ -0,0 +1,39 @@
+import 'dart:convert';
+
+class Article {
+  String author;
+  String title;
+  String description;
+  String url;
+  String urlToImage;
+  String publishedAt;
+  String content;
+
+  Article(
+      {this.author,
+      this.title,
+      this.description,
+      this.url,
+      this.urlToImage,
+      this.publishedAt,
+      this.content});
+
+  Article.fromJson(Map<String, dynamic> json) {
+    author = json["author"];
+    title = json["title"];
+    description = json["description"];
+    url = json["url"];
+    urlToImage = json["urlToImage"];
+    publishedAt = json["publishedAt"];
+    content = json["content"];
+  }
+}
+
+List<Article> parseArticles(String json) {
+  if (json == null) {
+    return [];
+  }
+
+  final List parsed = jsonDecode(json);
+  return parsed.map((json) => Article.fromJson(json)).toList();
+}
